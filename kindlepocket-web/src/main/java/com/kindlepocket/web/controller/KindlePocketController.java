78a0bdfae87c4d9aa6ef713f3604d0b37434b2a6
pocketContributor <shanian713@gmail.com>
2016-04-26 23:31:52 +0800

    [nasuf]

--- kindlepocket-web/src/main/java/com/kindlepocket/web/controller/KindlePocketController.java
+++ kindlepocket-web/src/main/java/com/kindlepocket/web/controller/KindlePocketController.java
@@ -12,7 +12,6 @@ import org.apache.log4j.Logger;
 import org.dom4j.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +25,6 @@ import com.kindlepocket.web.util.CheckUtil;
 import com.kindlepocket.web.util.MessageUtil;
 
 @RestController
-@SpringBootApplication
 @RequestMapping("/Weixin")
 public class KindlePocketController {
 
