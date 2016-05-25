4b42e1ea7aa3e136934048890e3cdb0a777d63c0
ken <shanian713@gmail.com>
2016-05-25 23:53:15 +0800

    1.modified the structure of files 2.init angular module and controller for test.
    Ken

---
+++ kindlepocket-web/src/main/resources/static/js/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by mac on 16/5/25.
+ */
+'use strict';
+
+var myApp = angular.module("myApp",[]);
---
+++ kindlepocket-web/src/main/resources/static/js/controller/myController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mac on 16/5/25.
+ */
+'use strict';
+
+
+myApp.controller('myController',['$scope',function($scope){
+    
+    $scope.title = "Kindle Your Kindle";
+    
+}]);
