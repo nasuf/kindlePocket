4337bcbf0d86f90a2b3b1c4c75543975173c0cf1
pocketContributor <shanian713@gmail.com>
2016-09-29 22:19:00 +0800

    [nasuf]

--- kindlepocket-cms/src/main/java/com/kindlepocket/cms/service/DeliveryRecordRepository.java
+++ kindlepocket-cms/src/main/java/com/kindlepocket/cms/service/DeliveryRecordRepository.java
@@ -1,12 +1,20 @@
 package com.kindlepocket.cms.service;
 
 import com.kindlepocket.cms.pojo.DeliveryRecord;
+import com.rabbitmq.client.AMQP;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Created by admin on 2016/9/4.
  */
 @Component
 public interface DeliveryRecordRepository extends MongoRepository<DeliveryRecord, String> {
+
+    List<DeliveryRecord> findBySubscriberOpenId(String subscriberOpenId);
+
+    DeliveryRecord findByTextBookId(ObjectId bookId);
 }
